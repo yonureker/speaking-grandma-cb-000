6558c7b437be97d74990e06aa6a105c17c06643d
Onur Eker <yonureker@gmail.com>
2017-12-15 23:44:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(stuff)
-  if stuff.Upcase == stuff
+  if stuff.upcase == stuff
     puts "NO, NOT SINCE 1938!"
   elsif stuff == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
