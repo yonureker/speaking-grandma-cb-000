29641942806a7062e36e0995263c9aa3755c71ca
Onur Eker <yonureker@gmail.com>
2017-12-15 23:48:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(stuff)
   if stuff.upcase != stuff
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   if stuff.upcase == stuff
     if stuff == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+      "I LOVE YOU TOO PUMPKIN!"
     else
-      puts "NO, NOT SINCE 1938!"
+      "NO, NOT SINCE 1938!"
     end
   end
 end
